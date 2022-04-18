44f6d3285adb4fbebe554687b72f3bb34b879fe7
María del Pilar Alegre Ramos <malegrera@uoc.edu>
2022-04-18 13:17:45 +0200

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'holamundo';
+  persona= {
+    'nombre': 'Pili',
+    'apellido': 'Alegre',
+    'fecnac': new Date()
+  }
+ fechas:Array<Date>=[0,1,2,3,4,5,6,7,8,9,10,11].map((i)=>{return new Date(2022,i,1);});
+
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID,NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import localeEs from '@angular/common/locales/es'
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localeEs,'es');
 
 @NgModule({
   declarations: [
@@ -12,7 +17,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [{provide: LOCALE_ID,useValue: 'es'}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
